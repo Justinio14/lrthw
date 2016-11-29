a406984fca8d962c074841fddf001c6f48b0b519
Justin Butwell <jbutwell@hotmail.com>
2016-11-29 11:59:21 +0000

    Exercise 3 Study drill

--- ex3.1.rb
+++ ex3.1.rb
@@ -1,26 +1,16 @@
-# + plus
-# - minus
-# / slash
-# * asterisk
-# % percent
-# < less-than
-# > greater-than
-# <= less-than-equal
-# >= gretaer-than-equal
-
 puts "I will now count my chickens:"
 
-puts "Hens #{25 + 30 / 6}"
+puts "Hens #{25.0 + 30.0 / 6.0}"
 
 # As a result of precedence (30 / 6) + 25 = 30
 
-puts "Roosters #{100 - 25 * 3 % 4}"
+puts "Roosters #{100.0 - 25.0 * 3.0 % 4.0}"
 
 # As a resut of precedence ((25 * 3) % 4) - 100 = 97
 
 puts "Now i will count the eggs:"
 
-puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+puts 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
 
 # As a result of precedence 3 + 2 + 1 - 5 + (4 % 2) - (1 / 4) + 6 = 7
 
@@ -28,15 +18,15 @@ puts "Is it true that 3 + 2 < 5 - 7?"
 
 # will write out text
 
-puts 3 + 2 < 5 - 7
+puts 3.0 + 2.0 < 5.0 - 7.0
 
 # will return false
 
-puts "What is 3 + 2? #{3 + 2}"
+puts "What is 3 + 2? #{3.0 + 2.0}"
 
 # will return 5 at the end of the string
 
-puts "What is 5 - 7? #{5 - 7}"
+puts "What is 5 - 7? #{5.0 - 7.0}"
 
 # Will return -2 at the end of the string
 
@@ -48,14 +38,14 @@ puts "How about some more."
 
 # writes string
 
-puts "Is it greater? #{5 > -2}"
+puts "Is it greater? #{5.0 > -2.0}"
 
 # Will return true at the end of the string
 
-puts "Is it greater or equal? #{5 >= -2}"
+puts "Is it greater or equal? #{5.0 >= -2.0}"
 
 # Will return true at the end of the string
 
-puts "Is it less or equal? #{5 <= -2}"
+puts "Is it less or equal? #{5.0 <= -2.0}"
 
 # Will return false at the end of the string
