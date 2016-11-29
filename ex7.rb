a036aa01b9dc3d3da23413d3b584634d219e6a95
Justin Butwell <jbutwell@hotmail.com>
2016-11-29 16:25:57 +0000

    Exercise 7 More Printing

---
+++ ex7.rb
@@ -0,0 +1,20 @@
+puts "Mary had a little lamb."
+puts "Its fleece was white as #{'snow'}."
+puts "And everwhere that Mary went."
+puts "." * 10 # what'd that do? ..........
+
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# Print will keep the data on the same line. Puts will put data on a new line
+print end1 + end2 + end3 + end4 + end5 + end6 + end7 + end8 + end9 + end10 + end11 + end12
