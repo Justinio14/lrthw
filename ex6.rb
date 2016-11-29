27240b2a081c47f126c3a9dad05f8ea536573340
Justin Butwell <jbutwell@hotmail.com>
2016-11-29 16:13:09 +0000

    Exercise 6 Strings and Text

---
+++ ex6.rb
@@ -0,0 +1,36 @@
+# Variable types_of_10 is created
+types_of_people = 10
+# creating a variable that is a string that contains another variable
+x = "There are #{types_of_people} types of people."
+# creating a variable that contains a string
+binary = "binary"
+# creating a variable that contains a string
+do_not = "don't"
+# creating a variable that is a string that contains 2 variables
+y = "Those who know #{binary} and those who #{do_not}."
+
+# writes out the value of x
+puts x
+# writes out the value of y
+puts y
+
+# writes a string containing variable x
+puts "I said: #{x}."
+# writes a string containing variable y
+puts "I also said: '#{y}'."
+
+# creating a variable set to false
+hilarious = false
+# creating variable that is a string which includes another variable
+joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
+
+# writes out variable joke_evaluation
+puts joke_evaluation
+
+# creating a variable that is a string
+ w = "This is the left side of..."
+ # creating a variable that is a string
+ e = "a string with a right side."
+
+ # concatenating and writing variables to screen
+ puts w + e
