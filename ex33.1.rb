1fe725ef74972cc398062779381ed29ef50dcb4d
Justin Butwell <jbutwell@hotmail.com>
2016-12-02 21:50:28 +0000

    Exercise 33 While Loops

---
+++ ex33.1.rb
@@ -0,0 +1,20 @@
+def loopy(x, y)
+i = 0
+numbers = []
+
+while i < x
+  puts "At the top i is #{i}"
+  numbers.push(i)
+
+  i += y
+  puts "Numbers now: ", numbers
+  puts "At the bottom i is #{i}"
+end
+
+puts "The numbers: "
+
+# remember you can write this 2 other ways?
+numbers.each {|num| puts num}
+end
+
+loopy(10, 2)
