ccbdeae57103048250e00acb37c54f17b1b39b6d
Justin Butwell <jbutwell@hotmail.com>
2016-12-03 21:14:14 +0000

    Exercise 34 Accessing Elements of Arrays

---
+++ ex34.rb
@@ -0,0 +1,12 @@
+# Remember: ordinal == ordered, 1st; cardinal == cards at random, 0.
+
+animals = ['bear', 'ruby', 'peacock', 'kangeroo', 'whale', 'platypus']
+
+# 1. the animal at 1 = 'ruby'(ordinal)
+# 2. the third (3rd) animal = 'peacock' (cardinal)
+# 3. the first animal = bear (ordinal)
+# 4. the animal at 3 = kangeroo (cardinal)
+# 5. the fifth animal = whale (ordinal)
+# 6. the animal at 2 peacock (cardinal)
+# 7. the sixth animal = platypus (ordinal)
+# 8. the animal at 4 = whale (cardinal)
